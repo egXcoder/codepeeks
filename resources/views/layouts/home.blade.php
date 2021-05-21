b833906e50ed27bed88bf1f829036606711f8bf1
Ahmed Ibrahim <ah.ib94@hotmail.com>
2021-05-21 04:53:32 +0200

    compile styles as production

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CodePeek</title>
-    <link rel="stylesheet" href="{{asset('css/home.css')}}">
+    <link rel="stylesheet" href="{{mix('css/home.css')}}">
 </head>
 
 <body>
