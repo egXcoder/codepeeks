b102361c039df5ba2752cb8b8cf80c0d2cf83dbf
Ahmed Ibrahim <ah.ib94@hotmail.com>
2021-05-22 04:17:21 +0200

    add google analytics

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -41,6 +41,15 @@
         </footer>
     </div>
     <script src="{{mix('js/home.js')}}"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-1VE98Y6RMF"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-1VE98Y6RMF');
+    </script>
 </body>
 
 </html>
