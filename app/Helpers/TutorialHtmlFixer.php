ed0b40690f0164e85ca8ba3be93f09f64a919f2b
Ahmed Ibrahim <ah.ib94@hotmail.com>
2021-05-22 04:08:06 +0200

    fix vscode block on saving tutorials html

--- app/Helpers/TutorialHtmlFixer.php
+++ app/Helpers/TutorialHtmlFixer.php
@@ -14,11 +14,12 @@ class TutorialHtmlFixer
     public function fix()
     {
         $this->fixToSafeQuotes();
+        $this->fixStylesOfCodeTakenFromVscode();
         return $this->description;
     }
 
     /**
-     * alter &quot; to be single quote ' 
+     * alter &quot; to be single quote '
      * as single quote is safer in render than double quote
      * because double quote may interfer with html attributes such
      * <span style="font-family:&quot;Courier&quot;"></span>
@@ -28,6 +29,13 @@ class TutorialHtmlFixer
     {
         $this->description = preg_replace_callback('/&quot;/', function ($matches) {
             return '\'';
+        }, $this->description);
+    }
+
+    protected function fixStylesOfCodeTakenFromVscode()
+    {
+        $this->description = preg_replace_callback('/<div style="color: rgb\(191, 199, 213\);/', function ($matches) {
+            return '<div style="padding:1rem;overflow:auto;color: rgb(191, 199, 213);';
         }, $this->description);
     }
 }
